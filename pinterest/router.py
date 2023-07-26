12a4044fa415d582f9d8947a55de731630e4708e
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-07-26 19:04:43 -0300

    initial project clone pintrest flask

---
+++ pinterest/router.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/perfil")
+def perfil():
+    return render_template("perfil.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)

4f74e3cc98f59dfe65c0023a73fa8924a738eab0
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-07-28 11:36:31 -0300

    Update routes.py

--- pinterest/routes.py
+++ pinterest/routes.py
@@ -3,10 +3,19 @@
 from flask import Flask, render_template, url_for
 from pinterest import app
 from flask_login import login_required
+from pinterest.forms import FormLogin, FormCriarConta
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def homepage():
-    return render_template("homepage.html")
+    formlogin = FormLogin()
+    return render_template("homepage.html", formlogin=formlogin)
+
+@app.route("/criar-conta", methods=["GET", "POST"])
+def criar_conta():
+    formcriarconta = FormCriarConta()
+    return render_template("criar-conta.html", formcriarconta=formcriarconta)
+
 
 @app.route("/perfil/<usuario>")
 @login_required

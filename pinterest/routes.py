778f838d5fda03cf8f79791e9b2346dbd93e1507
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-07-28 13:52:12 -0300

    Update routes.py

--- pinterest/routes.py
+++ pinterest/routes.py
@@ -1,20 +1,30 @@
 # criar as rotas do nosso site (os links)
 
 from flask import Flask, render_template, url_for
-from pinterest import app
+from pinterest import app, bcrypt, database
 from flask_login import login_required
 from pinterest.forms import FormLogin, FormCriarConta
+from pinterest.models import Usuario
 
 
 @app.route("/", methods=["GET", "POST"])
 def homepage():
-    formlogin = FormLogin()
-    return render_template("homepage.html", formlogin=formlogin)
+    form_login = FormLogin()
+    return render_template("homepage.html", formlogin=form_login)
+
 
 @app.route("/criar-conta", methods=["GET", "POST"])
 def criar_conta():
-    formcriarconta = FormCriarConta()
-    return render_template("criar-conta.html", formcriarconta=formcriarconta)
+    form_criarconta = FormCriarConta()
+    if form_criarconta.validate_on_submit():
+        senha = bcrypt.generate_password_hash(form_criarconta.senha.data).decode("utf-8")
+        usuario = Usuario(usuario=form_criarconta.usuario.data,
+                          email=form_criarconta.email.data, senha=senha)
+        
+        database.session.add(usuario)
+        database.session.commit()   
+    
+    return render_template("criar-conta.html", formcriarconta=form_criarconta)
 
 
 @app.route("/perfil/<usuario>")

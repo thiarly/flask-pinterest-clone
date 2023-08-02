2209caedd8abc0bb61bb889d136598e09a80199c
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-08-02 11:58:04 -0300

    Update __init__.py
    criando o engine e inspector para criar as tabelas no banco de dados do railway

--- pinterest/__init__.py
+++ pinterest/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 import os
-
+import sqlalchemy
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "b65ef8da938312e02784e9c0f37c955e3d030f52cdb7afe3c9c913190223"
@@ -19,6 +19,20 @@ database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "homepage"
+
+
+from pinterest.models import Usuario, Foto
+
+engine = sqlalchemy.create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table('usuario'):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("Base de dados criada com sucesso!")
+else:
+    print("Base de dados já existe!")
+
 
 
 from pinterest import routes

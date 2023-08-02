26701e4bd8b62b72af18dcc4ac768b0f8c50c0b4
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-08-02 14:20:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from pinterest import app
+from pinterest import app, database
 
+with app.app_context():
+    database.create_all()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=70, debug=True)

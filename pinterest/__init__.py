606a3fbf513362f8c07dcd9273c1209f628813cd
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-08-02 16:03:32 -0300

    Update __init__.py

--- pinterest/__init__.py
+++ pinterest/__init__.py
@@ -7,11 +7,11 @@ import sqlalchemy
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "b65ef8da938312e02784e9c0f37c955e3d030f52cdb7afe3c9c913190223"
+
 if os.getenv('DATABASE_URL'):
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
 else:
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pinterest.db"
-
 
 app.config["UPLOAD_FOLDER"] = "static/image/fotos_post/"
 
@@ -20,23 +20,19 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "homepage"
 
-
 from pinterest import routes
-
-
-
-
 from pinterest.models import Usuario
 
-engine = sqlalchemy.create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
-inspector = sqlalchemy.inspect(engine)
-if not inspector.has_table('usuario'):
-    with app.app_context():
-        database.drop_all()
-        database.create_all()
-        print("Base de dados criada com sucesso!")
-else:
-    print("Base de dados já existe!")
+def create_database_if_not_exists():
+    engine = sqlalchemy.create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+    inspector = sqlalchemy.inspect(engine)
+    if not inspector.has_table('usuario'):
+        with app.app_context():
+            database.create_all()
+            print("Base de dados criada com sucesso!")
+    else:
+        print("Base de dados já existe!")
 
+create_database_if_not_exists()
 
 

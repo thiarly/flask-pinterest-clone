f2bc0ba2e8c0635a0b734a95aba4620ef3f9f302
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-08-02 11:39:05 -0300

    Update __init__.py
    criando variável de ambiente para banco de dados do raiway

--- pinterest/__init__.py
+++ pinterest/__init__.py
@@ -2,13 +2,22 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pinterest.db"
+
 app.config["SECRET_KEY"] = "b65ef8da938312e02784e9c0f37c955e3d030f52cdb7afe3c9c913190223"
+
+if os.getenv('DATABASE_URL'):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pinterest.db"
+
+
+
 app.config["UPLOAD_FOLDER"] = "static/image/fotos_post/"
 
 
